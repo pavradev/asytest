6377a9d8528a7d72ed65949698ef5cb80f08c405
Pavel Rabetski <rabeckijps@gmail.com>
2023-04-30 18:21:40 +0200

    Add fixtures

--- atest.py
+++ atest.py
@@ -3,12 +3,12 @@ from functools import wraps
 import os
 import sys
 import time
-import textwrap
 import asyncio
 from importlib.util import spec_from_file_location, module_from_spec
 import inspect
 import traceback
-from typing import List, Optional
+from types import ModuleType
+from typing import Any, Callable, Dict, List, Optional
 from datetime import timedelta
 
 if len(sys.argv) != 2:
@@ -22,13 +22,16 @@ def get_python_files(path):
     """
     Reads all Python files from the given directory path.
     """
-    # List all files in the directory
-    file_list = os.listdir(path)
-    # Filter the list to only include .py files
-    py_files = filter(lambda x: x.endswith('.py'), file_list)
-    # Loop through the Python files and yield their paths
-    for py_file in py_files:
-        yield os.path.join(path, py_file)
+    # Load one file or list all files in the directory
+    if path.endswith('.py'):
+        yield path
+    else:
+        file_list = os.listdir(path)
+        # Filter the list to only include .py files
+        py_files = filter(lambda x: x.endswith('.py'), file_list)
+        # Loop through the Python files and yield their paths
+        for py_file in py_files:
+            yield os.path.join(path, py_file)
 
 
 def load_script_from_location(location):
@@ -100,7 +103,7 @@ def wrap_test_func(file_location, func_name, test_func):
     return test_func_wrapper
 
 def print_test_results(test_results: List[TestResult]) -> None:
-    print(f"{'='*33} TEST RESULTS {'='*33}\n")
+    print(format_line(" TEST RESULTS "))
     for result in test_results:
         exec_time_formatted = "{:.1f}".format(result.exec_time)
         name = result.name
@@ -108,6 +111,7 @@ def print_test_results(test_results: List[TestResult]) -> None:
         status = result.status.value
         color = "\033[92m" if status == "SUCCESS" else "\033[91m"
         print(f"{module}::{name} {color}{status:<10}\033[0m [{exec_time_formatted}s]")
+    print()
 
 def print_summary(results: List[TestResult], total_exec_time) -> None:
     """
@@ -120,42 +124,79 @@ def print_summary(results: List[TestResult], total_exec_time) -> None:
     num_passed = sum(1 for r in results if r.status == TestStatus.SUCCESS)
     num_failed = num_tests - num_passed
     total_time_formatted = str(timedelta(seconds=round(total_exec_time)))
-    status_line = f"{'='*80}\n"
+    status_line = " "
     if num_failed > 0:
         status_line += f"\033[31m{num_failed} failed\033[0m, "
-    status_line += f"\033[32m{num_passed} passed\033[0m in {total_exec_time:.1f}s ({total_time_formatted})\n"
-    status_line += f"{'='*80}\n"
-    print(textwrap.fill(status_line, width=80))
+    status_line += f"\033[32m{num_passed} passed\033[0m in {total_exec_time:.1f}s ({total_time_formatted}) "
+    print(format_line(" SUMMARY "))
+    print(status_line)
 
 def print_errors(test_results: List[TestResult]) -> None:
     """
     Print the errors for a list of test results.
     """
-    print(f"{'='*35} FAILURES {'='*35}\n")
+    print(format_line(" FAILURES "))
     for result in test_results:
         if result.status != TestStatus.SUCCESS:
             print(f"{result.module}::{result.name}: {type(result.error).__name__}")
             print(result.error)
             traceback.print_tb(result.error.__traceback__)
-            print(f"{'='*80}")
+            print(format_line(""))
 
+def format_line(line: str, line_size: int = 80) -> str:
+    if line_size <= len(line):
+        return line
+    else:
+        padding = line_size - len(line)
+        left_padding = padding // 2
+        right_padding = padding - left_padding
+        return f"{'='*left_padding}{line}{'='*right_padding}"
 
-async def run_tests():
-    all_test_functions = []
-    for file_location in get_python_files(tests_path):
-        t_module = load_script_from_location(file_location)
-        # Get all functions from the module
-        module_functions = inspect.getmembers(t_module, inspect.isfunction)
+def validate_is_async_generator(func_name, func):
+    if not inspect.isasyncgenfunction(func):
+        raise ValueError(f"Fixture function {func_name} is not async generator")
+
+def load_fixtures(module: ModuleType) -> List[Callable[..., Any]]:
+        module_functions = inspect.getmembers(module, inspect.isfunction)
+        # Filter the functions to only include those that start with "fixture_"
+        fixture_functions = [func for func in module_functions if func[0].startswith("fixture_")]
+        for func_name, func in fixture_functions:
+            validate_is_async_generator(func_name, func)
+        return fixture_functions
+
+def load_test_functions(module: ModuleType) -> List[Callable[..., Any]]:
+            # Get all functions from the module
+        module_functions = inspect.getmembers(module, inspect.isfunction)
         # Filter the functions to only include those that start with "test_"
         test_functions = [func for func in module_functions if func[0].startswith("test_")]
+        for func_name, func in test_functions:
+            validate_function_async(func_name, func)
+        return test_functions
+
+async def run_tests():
+    modules = [load_script_from_location(f) for f in get_python_files(tests_path)]
+
+    all_fixtures = {}
+    for m in modules:
+        for func_name, func in load_fixtures(m):
+            if func_name in all_fixtures:
+                raise ValueError(f"Duplicate fixture name: {func_name}")
+            all_fixtures[func_name] = func
+
+    all_test_functions = []
+    for m in modules:
+        test_functions = load_test_functions(m)
         # Call each test function
         for func_name, func in test_functions:
             validate_function_async(func_name, func)
             all_test_functions.append(wrap_test_func(
-                file_location=file_location, 
+                file_location=m.__name__, 
                 func_name=func_name, 
                 test_func=func
             ))
+
+    print(all_fixtures)
+
     start = time.time()
     test_results = await asyncio.gather(*[tf() for tf in all_test_functions])
     exec_time = time.time() - start
@@ -164,4 +205,5 @@ async def run_tests():
     print_summary(results=test_results, total_exec_time=exec_time)
 
 
-asyncio.run(run_tests())
+if __name__ == "__main__":
+    asyncio.run(run_tests())
