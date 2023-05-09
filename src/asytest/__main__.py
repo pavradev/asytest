dd9ddb3d8d023fbe17a9946eea874e55de01f08c
Pavel Rabetski <pavel.rabetski@consultant.volvo.com>
2023-05-09 08:41:27 +0200

    Issue #1 fix exit code on test failures

--- src/asytest/__main__.py
+++ src/asytest/__main__.py
@@ -3,4 +3,6 @@ from .asytest import parse_args, run_tests
 
 if __name__ == "__main__":
     args = parse_args()
-    run_tests(args.resource, args.max_concurrent)
+    test_suite_result = run_tests(args.resource, args.max_concurrent)
+    if test_suite_result.count_num_failed() > 0:
+        exit(1)
